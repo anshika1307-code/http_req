7ee57c871ad874c2a7e2af6e81ea1788760d778d
Anshika Goel <anshika1307goyal@gmail.com>
2024-03-08 22:11:11 +0530

    up

--- src/http_req_backend/src/lib.rs
+++ src/http_req_backend/src/lib.rs
@@ -1,4 +1,130 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+//all used method declaration 
+// relevant libraries and modules from the ic_cdk crate to facilitate canister management and HTTP request handling
+use ic_cdk::api::management_canister::http_request::{
+    http_request, CanisterHttpRequestArgument, HttpHeader, HttpMethod, HttpResponse, TransformArgs,
+    TransformContext,
+};
+
+use ic_cdk_macros::{self, query, update};
+use serde::{Serialize, Deserialize};
+use serde_json::{self, Value};
+//usd exchange?? later
+//http request and response
+#[ic_cdk::update]
+async fn get_icp_usd_exchange() -> String {
+    //ARGUMENTS FOR HTTP GET request
+    //the URL and its query parameters
+    type Timestamp = u64;
+    let start_timestamp: Timestamp = 1709676385; 
+    let seconds_of_time: u64 = 60; //start with 60 sec
+    let host = "api.pro.coinbase.com";
+    let url = format!(
+        "https://{}/products/ICP-USD/candles?start={}&end={}&granularity={}",
+        host,
+        start_timestamp.to_string(),
+        start_timestamp.to_string(),
+        seconds_of_time.to_string()
+    );
+
+    //header
+    let request_headers = vec![
+        HttpHeader {
+            name: "Host".to_string(),
+            value: format!("{host}:443"),
+        },
+        HttpHeader {
+            name: "User-Agent".to_string(),
+            value: "exchange_rate_canister".to_string(),
+        },
+    ];
+     
+    //request def
+    let request = CanisterHttpRequestArgument {
+        url: url.to_string(),
+        method: HttpMethod::GET,
+        body: None,              
+        max_response_bytes: None, 
+        transform: Some(TransformContext::new(transform, serde_json::to_vec(&context).unwrap())),
+        headers: request_headers,
+    };
+
+    match http_request(request).await {
+        //DECODE AND RETURN THE RESPONSE
+
+       
+        Ok((response,)) => {
+           //   //if successful, `HttpResponse` has this structure:
+            // pub struct HttpResponse {
+            //     pub status: Nat,
+            //     pub headers: Vec<HttpHeader>,
+            //     pub body: Vec<u8>,
+            // }
+//this comment is not understood (see this later)
+            //We need to decode that Vec<u8> that is the body into readable text.
+            //To do this, we:
+            //  1. Call `String::from_utf8()` on response.body
+            //  3. We use a switch to explicitly call out both cases of decoding the Blob into ?Text
+            let str_body = String::from_utf8(response.body)
+                .expect("Transformed response is not UTF-8 encoded.");
+
+            //Return the body and end 
+            str_body
+        }
+        Err((r, m)) => {
+            let message =
+                format!("The http_request resulted into error. RejectionCode: {r:?}, Error: {m}");
+
+            //error and end 
+            message
+        }
+    }
+}
+
+
+//unnecessary data out
+#[query]
+fn transform(raw: TransformArgs) -> HttpResponse {
+
+    let headers = vec![
+        HttpHeader {
+            name: "Content-Security-Policy".to_string(),
+            value: "default-src 'self'".to_string(),
+        },
+        HttpHeader {
+            name: "Referrer-Policy".to_string(),
+            value: "strict-origin".to_string(),
+        },
+        HttpHeader {
+            name: "Permissions-Policy".to_string(),
+            value: "geolocation=(self)".to_string(),
+        },
+        HttpHeader {
+            name: "Strict-Transport-Security".to_string(),
+            value: "max-age=63072000".to_string(),
+        },
+        HttpHeader {
+            name: "X-Frame-Options".to_string(),
+            value: "DENY".to_string(),
+        },
+        HttpHeader {
+            name: "X-Content-Type-Options".to_string(),
+            value: "nosniff".to_string(),
+        },
+    ];
+    
+
+    let mut res = HttpResponse {
+        status: raw.response.status.clone(),
+        body: raw.response.body.clone(),
+        headers,
+        ..Default::default()
+    };
+
+    if res.status == 200 {
+
+        res.body = raw.response.body;
+    } else {
+        ic_cdk::api::print(format!("Received an error from coinbase: err = {:?}", raw));
+    }
+    res
 }
